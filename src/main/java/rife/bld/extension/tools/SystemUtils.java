659b21d8060adcd3fc30db55a99fa0395823eba2
Erik C. Thauvin <erik@thauvin.net>
2026-01-03 00:37:33 -0800

    Add since annotations to OS detection methods

--- src/main/java/rife/bld/extension/tools/SystemUtils.java
+++ src/main/java/rife/bld/extension/tools/SystemUtils.java
@@ -40,6 +40,7 @@ public final class SystemUtils {
      * @param osName The name of the operating system to evaluate
      * @return {@code true} if the operating system name contains "aix" (case-insensitive),
      * {@code false} otherwise
+     * @since 1.0
      */
     static boolean isAix(String osName) {
         var normalized = normalizeOSName(osName);
@@ -50,6 +51,7 @@ public final class SystemUtils {
      * Determines if the current operating system is AIX.
      *
      * @return {@code true} if the operating system is identified as AIX, {@code false} otherwise
+     * @since 1.0
      */
     public static boolean isAix() {
         return isAix(OS_NAME);
@@ -71,6 +73,7 @@ public final class SystemUtils {
      * @param osName      The name of the operating system
      * @param envProvider A function to retrieve environment variables
      * @return {@code true} if the environment is detected as Cygwin, {@code false} otherwise
+     * @since 1.0
      */
     static boolean isCygwin(String osName, Function<String, String> envProvider) {
         // Check if OS is Windows first
@@ -102,7 +105,8 @@ public final class SystemUtils {
      * Determines if the given operating system name corresponds to FreeBSD.
      *
      * @param osName The name of the operating system to evaluate.
-     * @return {@code true} if the operating system is FreeBSD, {@code false} otherwise
+     * @return {@code true} if the operating system is FreeBSD, {@code false} otherwise\
+     * @since 1.0
      */
     static boolean isFreeBsd(String osName) {
         var normalized = normalizeOSName(osName);
@@ -112,7 +116,8 @@ public final class SystemUtils {
     /**
      * Determines if the current operating system is FreeBSD.
      *
-     * @return {@code true} if the operating system is FreeBSD, {@code false} otherwise
+     * @return {@code true} if the operating system is FreeBSD, {@code false} otherwise\
+     * @since 1.0
      */
     public static boolean isFreeBsd() {
         return isFreeBsd(OS_NAME);
@@ -122,6 +127,7 @@ public final class SystemUtils {
      * Determines if the operating system is Linux.
      *
      * @return {@code true} if the operating system is Linux, {@code false} otherwise
+     * @since 1.0
      */
     public static boolean isLinux() {
         return isLinux(OS_NAME);
@@ -133,6 +139,7 @@ public final class SystemUtils {
      * @param osName The name of the operating system to evaluate
      * @return {@code true} if the operating system name contains "linux" or "unix" (case-insensitive),
      * {@code false} otherwise
+     * @since 1.0
      */
     static boolean isLinux(String osName) {
         var normalized = normalizeOSName(osName);
@@ -143,6 +150,7 @@ public final class SystemUtils {
      * Determines if the current operating system is macOS.
      *
      * @return {@code true} if the OS is macOS, {@code false} otherwise
+     * @since 1.0
      */
     public static boolean isMacOS() {
         return isMacOS(OS_NAME);
@@ -154,6 +162,7 @@ public final class SystemUtils {
      * @param osName The name of the operating system to evaluate
      * @return {@code true} if the operating system name contains "mac", "darwin", or "osx" (case-insensitive),
      * {@code false} otherwise
+     * @since 1.0
      */
     static boolean isMacOS(String osName) {
         var normalized = normalizeOSName(osName);
@@ -164,6 +173,7 @@ public final class SystemUtils {
      * Determines if the current environment is running in a Mingw environment.
      *
      * @return {@code true} if the environment is detected as MinGW/MSYS2, {@code false} otherwise
+     * @since 1.0
      */
     public static boolean isMingw() {
         return isMingw(OS_NAME, System::getenv);
@@ -176,6 +186,7 @@ public final class SystemUtils {
      * @param osName      The name of the operating system
      * @param envProvider A function to retrieve environment variables
      * @return {@code true} if the environment is detected as MinGW/MSYS2, {@code false} otherwise
+     * @since 1.0
      */
     static boolean isMingw(String osName, Function<String, String> envProvider) {
         // Check if OS is Windows first
@@ -218,6 +229,7 @@ public final class SystemUtils {
      * Determines if the current operating system is OpenVMS.
      *
      * @return {@code true} if the operating system is OpenVMS, {@code false} otherwise
+     * @since 1.0
      */
     public static boolean isOpenVms() {
         return isOpenVms(OS_NAME);
@@ -228,6 +240,7 @@ public final class SystemUtils {
      *
      * @param osName The name of the operating system to evaluate
      * @return {@code true} if the operating system name contains "openvms" (case-insensitive), {@code false} otherwise
+     * @since 1.0
      */
     static boolean isOpenVms(String osName) {
         var normalized = normalizeOSName(osName);
@@ -240,6 +253,7 @@ public final class SystemUtils {
      *
      * @return {@code true} if the operating system is other than AIX, FreeBSD, Linux, macOS,
      * OpenVMS, Solaris, or Windows, {@code false} otherwise
+     * @since 1.0
      */
     public static boolean isOtherOS() {
         return isOtherOs(OS_NAME);
@@ -251,6 +265,7 @@ public final class SystemUtils {
      *
      * @return {@code true} if the operating system is other than AIX, FreeBSD, Linux, macOS, OpenVMS, Solaris,
      * or Windows, {@code false} otherwise
+     * @since 1.0
      */
     static boolean isOtherOs(String osName) {
         return !isAix(osName) && !isFreeBsd(osName) && !isLinux(osName) && !isMacOS(osName) && !isOpenVms(osName)
@@ -261,6 +276,7 @@ public final class SystemUtils {
      * Determines if the current operating system is Solaris.
      *
      * @return {@code true} if the operating system is Solaris, {@code false} otherwise
+     * @since 1.0
      */
     public static boolean isSolaris() {
         return isSolaris(OS_NAME);
@@ -272,6 +288,7 @@ public final class SystemUtils {
      * @param osName The name of the operating system to evaluate
      * @return {@code true} if the operating system name contains "solaris" or "sunos" (case-insensitive),
      * {@code false} otherwise
+     * @since 1.0
      */
     static boolean isSolaris(String osName) {
         var normalized = normalizeOSName(osName);
@@ -282,6 +299,7 @@ public final class SystemUtils {
      * Determines if the current operating system is Windows.
      *
      * @return {@code true} if the operating system is Windows, {@code false} otherwise
+     * @since 1.0
      */
     public static boolean isWindows() {
         return isWindows(OS_NAME);
@@ -291,6 +309,7 @@ public final class SystemUtils {
      * Determines if the given operating system name corresponds to Windows.
      *
      * @param osName The name of the operating system to evaluate
+     * @since 1.0
      */
     static boolean isWindows(String osName) {
         var normalized = normalizeOSName(osName);
@@ -304,6 +323,7 @@ public final class SystemUtils {
      * @param osName The name of the operating system to normalize
      * @return A normalized, lowercase version of the operating system name, or an
      * empty string if the input is {@code null}
+     * @since 1.0
      */
     static String normalizeOSName(String osName) {
         return osName != null ? osName.toLowerCase(Locale.ENGLISH) : "";
