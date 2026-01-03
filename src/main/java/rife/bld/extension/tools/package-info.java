4dff5cb83c982bc86e981344d078da78c2576c69
Erik C. Thauvin <erik@thauvin.net>
2026-01-02 19:03:19 -0800

    Add package info

---
+++ src/main/java/rife/bld/extension/tools/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Provides a collection of tools used by various
+ * <a href="https://github.com/rife2/bld/wiki/Extensions">bld Extensions</a>.
+ *
+ * @author <a href="https://erik.thauvin.net/">Erik C. Thauvin</a>
+ * @since 1.0
+ */
+package rife.bld.extension.tools;
