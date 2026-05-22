e47e176a78046d8aaa250e1808a7404340431ea9
Erik C. Thauvin <erik@thauvin.net>
2026-05-21 20:50:46 -0700

    Bump JUnit to version 6.1.0

--- src/bld/java/rife/bld/extension/tools/ToolsBuild.java
+++ src/bld/java/rife/bld/extension/tools/ToolsBuild.java
@@ -48,7 +48,7 @@ public class ToolsBuild extends Project {
 
         repositories = List.of(MAVEN_CENTRAL, RIFE2_RELEASES, RIFE2_SNAPSHOTS);
 
-        var junit = version(6, 0, 3);
+        var junit = version(6, 1, 0);
         scope(provided)
                 .include(dependency("com.github.spotbugs", "spotbugs-annotations",
                         version(4, 9, 8)));
