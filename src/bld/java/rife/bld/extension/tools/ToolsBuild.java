4a0d1f5926d3b5adf92fe7804b05b02ba6d2794a
Erik C. Thauvin <erik@thauvin.net>
2026-08-07 11:18:21 -0700

    Bump JUnit to version 6.1.3

--- src/bld/java/rife/bld/extension/tools/ToolsBuild.java
+++ src/bld/java/rife/bld/extension/tools/ToolsBuild.java
@@ -48,7 +48,7 @@ public class ToolsBuild extends Project {
 
         repositories = List.of(MAVEN_CENTRAL, RIFE2_RELEASES, RIFE2_SNAPSHOTS);
 
-        var junit = version(6, 1, 2);
+        var junit = version(6, 1, 3);
         scope(provided)
                 .include(dependency("com.uwyn.rife2", "bld-testing-helpers",
                         version(1, 1, 0, "SNAPSHOT")))
